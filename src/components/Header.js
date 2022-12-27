190e55c7a28d3b3bb6a274f1e5824be029e0e70d
Kyle Lorelli <kyle.lorelli@gmail.com>
2022-12-27 17:13:31 -0500

    Follow up commit to include new files

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import styles from './Header.module.css';
+
+const Header = () => {
+    return <header className={styles.header}>
+        <h1>memory palace</h1>
+    </header>
+}
+
+export default Header;
