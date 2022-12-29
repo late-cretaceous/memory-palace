96911539a937a9f4801fada8e2f0372c7d587a1c
Kyle Lorelli <kyle.lorelli@gmail.com>
2022-12-29 17:27:10 -0500

    Drag and drop properties added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,17 @@
-import styles from './Todo.module.css';
-import Card from './UI/Card';
+import styles from "./Todo.module.css";
+import Card from "./UI/Card";
 
-const Todo = props => {
-    const dragHandler = e => {
-        console.log()
-    };
-
-    return <Card className={styles.todo} draggable='true'>
-        <h3>{props.id}</h3>
+const Todo = (props) => {
+  return (
+    <Card
+      className={styles.todo}
+      draggable="true"
+      id={props.id}
+      onDragStart={props.onGrab}
+    >
+      <h3>{props.id}</h3>
     </Card>
-}
+  );
+};
 
 export default Todo;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,15 +5,41 @@ import { useState } from "react";
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
 
-  const todoComponentList = todos.map((todo) => <Todo id={todo} key={todo} />);
+  const dragStartHandler = (e) => {
+    e.dataTransfer.setData("text/plain", e.target.id);
+    e.dataTransfer.effectAllowed = "move";
+  };
+
+  const todoComponentList = todos.map((todo) => (
+    <Todo id={todo.id} key={todo.id} onGrab={dragStartHandler} />
+  ));
 
   const addTodoHandler = (e) => {
-    setTodos((previous) => previous.concat([previous.length + 1]));
+    setTodos((previous) => {
+      return previous.concat([{ id: previous.length + 1 }]);
+    });
+  };
+
+  console.log(todos);
+
+  const dragOverHandler = (e) => {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "move";
+  };
+
+  const dropHandler = (e) => {
+    e.preventDefault();
+    const data = e.dataTransfer.getData("text/plain");
+    console.log(data);
   };
 
   return (
     <div className={styles.flexcol} onClick={addTodoHandler}>
-      <ul className={`${styles.flexcol} ${styles.list}`}>
+      <ul
+        className={`${styles.flexcol} ${styles.list}`}
+        onDragOver={dragOverHandler}
+        onDrop={dropHandler}
+      >
         {todoComponentList}
       </ul>
     </div>
