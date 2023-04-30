e20ca977924c321c4df3cc42bb8ba4f578fa700b
Kyle Lorelli <kyle.lorelli@gmail.com>
2023-04-30 19:50:23 -0400

    Created HSL object to better manage a more generalized range function

--- src/utilities/style.js
+++ src/utilities/style.js
@@ -9,3 +9,55 @@ export const getShadesOfColor = ([hue, sat = 0, light = 0], steps) => {
 
   return shades;
 };
+
+export class HSL {
+  constructor(hue, sat, light) {
+    this.hue = hue;
+    this.sat = sat;
+    this.light = light;
+  }
+
+  hslRange(other, steps) {
+    const ranges = new HSL([], [], []);
+
+    for (const dimension in this) {
+      ranges[dimension] = divideRange(this[dimension], other[dimension], steps);
+    }
+
+    return Array.from(ranges.zipArrays(), (hsl) => hsl.toString());
+  }
+
+  toString() {
+    return `hsl(${this.hue}, ${this.sat}%, ${this.light}%`;
+  }
+
+  zipArrays() {
+    if (typeof this.hsl === "number") return this;
+
+    return Array.from(
+      { length: this.hue.length },
+      (_, i) => new HSL(this.hue[i], this.sat[i], this.light[i])
+    );
+  }
+}
+
+function divideRange(start, end, numDivisions) {
+  const divisionSize = (end - start) / numDivisions;
+
+  return Array.from(
+    { length: numDivisions + 1},
+    (_, i) => start + 1 + i * divisionSize
+  );
+}
+
+export const divideHSL = ([hue, sat = 0, light = 0], steps, options = {}) => {
+  const shades = [];
+  const satStep = (100 - sat) / steps;
+  const lightStep = (100 - light) / steps;
+
+  for (let i = 1; i <= steps; i++) {
+    shades.push(`hsl(${hue}, ${i * satStep + sat}%, ${i * lightStep + light}%`);
+  }
+
+  return shades;
+};
