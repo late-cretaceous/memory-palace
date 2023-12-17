e7712636015bfd974da9281632a2ef7b8806d7d1
Kyle Lorelli <kyle.lorelli@gmail.com>
2023-12-17 18:35:22 -0500

    Added toggling styles to sort button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 import styles from "./Header.module.css";
 import buttonStyle from "./UI/button.module.css";
 import { ReactComponent as Sort } from "../assets/sort.svg";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { sort } from "../redux/globalSlice";
 
 const Header = (props) => {
   const dispatch = useDispatch();
   const colorComplement = props.color.negative().toString();
+  const sorting = Boolean(
+    useSelector((state) => state.globalSlice.sort) !== "manual"
+  );
+  const buttonClasses = `${buttonStyle.button} ${
+    sorting ? styles.selected : styles.unselected
+  }`;
 
   return (
     <header
@@ -19,7 +25,7 @@ const Header = (props) => {
       <h1>neato note</h1>
       <div className={styles["header-buttons-row"]}>
         <button
-          className={buttonStyle.button}
+          className={buttonClasses}
           onClick={() => {
             dispatch(sort("date"));
           }}
