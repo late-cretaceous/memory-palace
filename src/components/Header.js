b090a88746ad9cbfbabfa2e238ee7b38c911d149
Kyle Lorelli <kyle.lorelli@gmail.com>
2023-12-16 19:05:43 -0500

    Added in a styles sort icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,20 @@
 import styles from "./Header.module.css";
-import { ReactComponent as Sort } from "../assets/triangle.svg";
+import { ReactComponent as Sort } from "../assets/sort.svg";
 
 const Header = (props) => {
+  const fontColor = props.color.negative().toString();
+
   return (
     <header
       className={styles.header}
       style={{
         backgroundColor: props.color,
-        color: props.color.negative().toString(),
+        color: fontColor,
       }}
     >
       <h1>neato note</h1>
       <div className={styles["header-buttons-row"]}>
-        <Sort />
+        <Sort className={styles.button} style={{ fill: fontColor }} />
       </div>
     </header>
   );
