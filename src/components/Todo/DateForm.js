bb6b85d02add7c2072e5daeb012d9740b5d3016d
Kyle Lorelli <kyle.lorelli@gmail.com>
2024-05-12 10:26:23 -0400

    Date animation is in place

--- src/components/Todo/DateForm.js
+++ src/components/Todo/DateForm.js
@@ -4,16 +4,15 @@ import { useState } from "react";
 
 const DateForm = ({ todo, ...props }) => {
   const [focused, setFocused] = useState(false);
-  const [inputWidths, setInputWidths] = useState({day: 0, month: 0, year: 0});
+  const [inputWidths, setInputWidths] = useState({ day: 0, month: 0, year: 0 });
   const [hovered, setHovered] = useState(false);
 
   const handleFocus = () => setFocused(true);
   const handleBlur = () => setFocused(false);
 
-  const formWidth = Object.values(inputWidths).reduce(
-    (acc, curr) => acc + curr,
-    0
-  );
+  const formWidth = hovered
+    ? Object.values(inputWidths).reduce((acc, curr) => acc + curr, 0)
+    : Object.values(inputWidths)[0];
   console.log(`formWidth: ${formWidth}`);
 
   const sendInputWidth = (name, width) => {
